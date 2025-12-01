3b8d5cf2bdbf26d097f3c9e00845119eb6ea8695
tZandr <alex_tjernstrom@hotmail.com>
2025-12-01 15:59:24 +0100

    Assignment finished

--- databasövningar-dag1/övning2.sql
+++ databasövningar-dag1/övning2.sql
@@ -32,10 +32,10 @@
 
 
 -- 8. Skapa en tabell för "skivor"
--- Tabellen skall kunna användas av t.ex. en butik för CD, LP-skivor, så att all nödvändig information om varje skiva finns med.
--- Skapa själv kolumner för tabellen, och välj rätt datatyp.
--- Man skall kunna hitta information om vad skivan heter, vad den kostar, när den är utgiven etc.
--- Du ska ha minst fem kolumner.
+    -- Tabellen skall kunna användas av t.ex. en butik för CD, LP-skivor, så att all nödvändig information om varje skiva finns med.
+    -- Skapa själv kolumner för tabellen, och välj rätt datatyp.
+    -- Man skall kunna hitta information om vad skivan heter, vad den kostar, när den är utgiven etc.
+    -- Du ska ha minst fem kolumner.
 
     CREATE TABLE skivor(id INT PRIMARY KEY, type VARCHAR(50), name VARCHAR(50), price DECIMAL(10,2), releaseDate DATE);
 
@@ -64,3 +64,43 @@
 -- 13. Skriv en SQL-fråga som visar alla skivor för en viss artist, visa bara kolumnerna releaseDate och namn.
     SELECT name, releaseDate FROM skivor
     WHERE name = 'Metallica'
+
+-- 14. Skapa en tabell för "Anställda"
+    -- Tabellen skall kunna användas av t.ex. Ett företag med många anställda, så att all nödvändig information om varje person finns med.
+    -- Skapa själv kolumner för tabellen, och välj rätt datatyp.
+    -- Man skall kunna hitta information om vad den anställde heter, vad hen har för lön, vad hen har för befattning etc.
+    -- Du ska ha minst fem kolumner.
+    -- Skriv SQL-kod med CREATE TABLE för att skapa tabellen. Mata in minst 2 rader i din tabell med INSERT INTO.
+
+    CREATE TABLE employees(id INT PRIMARY KEY, type VARCHAR(50), name VARCHAR(50), city VARCHAR(50), hireDate DATE);
+
+    INSERT INTO employees
+    VALUES (0001, 'Boss', 'Magdalene', 'Stockholm', '2001-08-12'),
+        (0002, 'Minion', 'Alice', 'Uppsala', '2015-11-30'),
+        (0003, 'Minion', 'Jonas', 'Göteborg', '2021-05-01'),
+        (0004, 'Minion', 'Burt', 'Malmö', '2025-01-30');
+
+-- 15. Skriv en SQL-fråga som hämtar alla anställda från tabellen ”anställda”, sorterat på senast inlagd.
+    SELECT * FROM employees ORDER BY hireDate DESC;
+
+-- 16. Skriv en SQL-fråga som raderar alla anställda från tabellen ”anställda” som bor i Göteborg.
+    DELETE FROM employees
+    WHERE city = 'Göteborg';
+
+-- 17. Skriv en SQL-fråga som uppdaterar någon av kolumnerna för alla anställda som är ”chefer”
+    UPDATE employees
+    SET type = 'SuperBoss'
+    WHERE type = 'Boss';
+
+-- 18. Lägg till en ny kolumn med ALTER i tabellen ”anställda” t ex avdelning.
+    ALTER TABLE employees
+    ADD Section VARCHAR(50);
+
+-- 19. Skriv sedan en SQL-fråga som gör en UPDATE för samtliga anställda så att de tillhör samma avdelning.
+    UPDATE employees
+    SET Section = 'Economy'
+    WHERE id >= 0
+
+-- 20. Skriv en SQL-fråga som lägger till en ny anställd med INSERT INTO. Den nya anställda ska tillhöra en ny avdelning.
+    INSERT INTO employees
+    VALUE (0005, 'Boss', 'Joppe', 'Göteborg', '2023-03-03', 'SocialMedia');
