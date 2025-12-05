25c1edb68764b21b06414430860406a7d30a3651
tZandr <alex_tjernstrom@hotmail.com>
2025-12-05 10:50:38 +0100

    Del 2

--- d2_databas,sql-övningar.sql
+++ d2_databas,sql-övningar.sql
@@ -68,3 +68,83 @@ WHERE orders.id = 005;
     -- Som raderar en rad i tabellen kund
 DELETE FROM kund
 WHERE kund.id = 004;
+
+
+-- Del 2: Många-till-många (order-produkt)
+    -- 5. Skapa tabellerna produkt och order_rad
+CREATE TABLE produkt(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    namn VARCHAR(50),
+    pris DECIMAL(10,2),
+    kategori VARCHAR(50)
+);
+
+CREATE TABLE order_rad (
+    orders_id   INT NOT NULL,
+    produkt_id INT NOT NULL,
+    antal      INT NOT NULL,
+    radpris    INT NOT NULL,
+    PRIMARY KEY (orders_id, produkt_id),
+    FOREIGN KEY (orders_id)   REFERENCES orders(id),
+    FOREIGN KEY (produkt_id) REFERENCES produkt(id)
+);
+
+    -- 6. Fyll tabellerna med data:
+        -- Minst 4 produkter
+        -- Lägg in order rader så att någon order har flera produkter, och någon produkt ingår i flera ordrar.
+INSERT INTO produkt
+VALUES (01, 'Vassleprotein', 299.00, 'kosttillskott'),
+       (02, 'Kaseinprotein', 319.00, 'kosttillskott'),
+       (03, 'Creatine Monohydrate', 199.00, 'kosttillskott'),
+       (04, 'T-Shirt', 399.00, 'kläder'),
+       (05, 'Byxor', 599.00, 'kläder');
+
+INSERT INTO order_rad
+VALUES (0001, 01, 5, 5 * 299), -- 5x vassle
+        (0001, 03, 5, 5 * 199), -- 5x creatine
+        (0002, 01, 2, 2* 299), -- 2x vassle(finns även i order 1)
+        (0002, 05, 2, 2 * 599) -- 2x byxor
+
+    -- 7. Skriv SQL frågor som:
+        -- visar alla ordrar med tillhörande produkter(orderid, kundnamn, produktnamn, antal)
+SELECT
+    r.order_id,
+    p.namn,
+    r.antal
+FROM order_rad r
+JOIN produkt p ON p.id = r.produkt_id
+
+        -- som visar alla produkter som ingår i en viss order
+SELECT
+    r.order_id,
+    p.namn,
+    r.antal,
+    r.radpris
+FROM order_rad r
+JOIN produkt p ON r.produkt_id = p.id
+WHERE r.order_id = 0001;
+
+        -- som visar alla produkter tillsammans med hur många gånger de beställts totalt. (tips: GROUP BY)
+SELECT
+    p.namn,
+    SUM(r.antal) AS totalt_beställt
+FROM order_rad r
+JOIN produkt p ON r.produkt_id = p.id
+GROUP BY p.id, p.namn
+
+        -- som visar alla produkter, även de som inte förekommer i någon order_rad
+SELECT
+    p.id,
+    p.namn,
+    p.pris,
+    p.kategori,
+    SUM(r.antal) AS totalt_beställda
+FROM produkt p
+LEFT JOIN order_rad r ON p.id = produkt_id
+GROUP BY p.id, p.namn;
+
+        -- som raderar en viss produkt ur tabellen produkt.
+        -- Fundera på vad som händer med kopplingarna, behöver man radera i order_rad först för att behålla referensintegriteten?
+DELETE FROM produkt
+WHERE produkt.id = 05; -- Ger error, svar ja. ^
+
