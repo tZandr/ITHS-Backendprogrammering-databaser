3898a36c34ee82a0fe77b7f0863c809246c53703
tZandr <alex_tjernstrom@hotmail.com>
2026-01-13 00:39:33 +0100

    Node.js for backend intro

---
+++ Exercises/Node.JS/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+res.send('You just posted cringe! You are going to lose subscriber.')
+})
+
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`);
+})
