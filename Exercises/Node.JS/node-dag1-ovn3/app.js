76584de1bc64d05d22d902132d2a4bd75a286b3c
tZandr <alex_tjernstrom@hotmail.com>
2026-01-16 07:58:47 +0100

    works kinda

--- Exercises/Node.JS/node-dag1-ovn3/app.js
+++ Exercises/Node.JS/node-dag1-ovn3/app.js
@@ -1,5 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/public", (req, res) => {
+  res.send("gpt thinks this is wrong:)");
+});
+
+app.post("/records", (req, res) => {
+  const { artist, title } = req.body;
+  res.send("jag heter " + artist);
+});
+
+app.use(express.static("public"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
