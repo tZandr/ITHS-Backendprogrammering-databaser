617d9056c78fa882792d66df780033de2e9c5743
tZandr <alex_tjernstrom@hotmail.com>
2026-01-31 21:33:29 +0100

    CRUD Routes for products

--- Exercises/Product-Supplier-API/routes/products.js
+++ Exercises/Product-Supplier-API/routes/products.js
@@ -8,8 +8,40 @@ router.post("/", async (req, res) => {
   const product = new Product(req.body);
   await product.save();
 
+  // Push product ID to supplier's products array
   await Supplier.findByIdAndUpdate(product.supplier, {
     $push: { products: product._id },
   });
   res.status(201).json(product);
+});
+
+// READ/GET all products
+router.get("/", async (req, res) => {
+  const products = await Product.find().populate("supplier");
+  res.json(products);
+});
+
+// READ/GET single product by ID
+router.get("/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id).populate("supplier");
+  res.json(product);
+});
+
+// UPDATE single product by ID
+router.put("/:id", async (req, res) => {
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json(product);
+});
+
+// DELETE single product by ID
+router.delete("/:id", async (req, res) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+
+  // remove from suppliers product array
+  await Supplier.findByIdAndDelete(product.supplier, {
+    $pull: { products: product._id },
+  });
+  res.json({ message: "Product deleted from supplier" });
 });
