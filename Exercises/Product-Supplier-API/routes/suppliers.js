b5b0eb5213f0785a24b75c3617217755bef3debc
tZandr <alex_tjernstrom@hotmail.com>
2026-01-31 21:50:36 +0100

    supplier CRUD

--- Exercises/Product-Supplier-API/routes/suppliers.js
+++ Exercises/Product-Supplier-API/routes/suppliers.js
@@ -2,3 +2,38 @@ const express = require("express");
 const router = express.Router();
 const Supplier = require("../models/supplier");
 const Product = require("../models/product");
+
+// POST: Skapa en ny leverantör
+router.post("/", async (req, res) => {
+  const supplier = new Supplier(req.body);
+  await supplier.save();
+});
+
+// GET: Hämta alla leverantörer
+router.get("/", async (req, res) => {
+  const supplier = await Supplier.find().populate("product");
+  res.json(supplier);
+});
+
+// GET: Hämta en specifik leverantör med ID
+router.get("/:id", async (req, res) => {
+  const supplier = await Supplier.find(req.params.id).populate("product");
+  res.json(supplier);
+});
+
+// PUT: Uppdatera en leverantör
+router.put("/:id", async (req, res) => {
+  const supplier = await Supplier.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json(supplier);
+});
+
+// DELETE: Ta bort en leverantör
+router.delete("/:id", async (req, res) => {
+  const supplier = await Supplier.findByIdAndDelete(req.params.id);
+  await product.findByIdAndDelete(supplier.product, {
+    $pull: { products: products._id },
+  });
+  res.json({ message: "Supplier deleted from product's supplier array" });
+});
