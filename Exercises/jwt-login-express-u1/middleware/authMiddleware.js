5a3be4eea12f67090f43b3054ec048b1dc0b0e06
tZandr <alex_tjernstrom@hotmail.com>
2026-02-09 09:34:56 +0100

    middleware, decoding jwt token

---
+++ Exercises/jwt-login-express-u1/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+export const protect = (req, res, next) => {
+  // Get the token from the Authorization header:
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).send("No token provided");
+  }
+  try {
+    // Verify the token and extract the user ID:
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (err) {
+    return res.status(401).send("Invalid token");
+  }
+};
